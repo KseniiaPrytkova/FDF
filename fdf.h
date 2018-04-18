dd43c2550a3c66ffb3e703c42093ad4fabcf6a3a
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-04-18 18:33:45 +0300

    preparations for work

---
+++ fdf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kprytkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/18 18:16:19 by kprytkov          #+#    #+#             */
+/*   Updated: 2018/04/18 18:16:20 by kprytkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+
+int		get_next_line(const int fd, char **line);
+#endif
