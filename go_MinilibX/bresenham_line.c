e64bc5efd7bd750067a068573f9bb068d7c105a0
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-05-08 20:02:57 +0300

    works strange

--- go_MinilibX/bresenham_line.c
+++ go_MinilibX/bresenham_line.c
@@ -21,7 +21,7 @@ int		ft_max(int d_x, int d_y, int max_d)
 	return (max_d);
 }
 
-void 	ft_bresenham(int x_0, int x_1, int y_0, int y_1, void *mlx_ptr, void *win_ptr)
+void 	ft_bresenham(int x_0, int y_0, int x_1, int y_1, void *mlx_ptr, void *win_ptr, int color)
 {
 	int 	dx;
 	int		dy;
@@ -34,35 +34,34 @@ void 	ft_bresenham(int x_0, int x_1, int y_0, int y_1, void *mlx_ptr, void *win_
 		s_x = 1;
 	else
 		s_x = 0;
-    
     dy = abs(y_1 - y_0);
-    
     if ((y_1 - y_0) >= 0)
 		s_y = 1;
 	else
 		s_y = 0; 
     max_delta = ft_max(dx, dy, max_delta);    
     if (max_delta == 0)
-		mlx_pixel_put(mlx_ptr, win_ptr, x_0, y_0, 8388352);
+		mlx_pixel_put(mlx_ptr, win_ptr, x_0, y_0, color);
 	if (dy <= dx)
 	{
+		printf("y <= dx\n");
 		int x = x_0;
 		int y = y_0;
 		int d = -dx;
 
-		max_delta = max_delta + 1;
-		while(max_delta != 0)
+		max_delta++;
+		while(max_delta--)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 8388352);
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+			printf("%d %d\n",x, y);
 			x = x + s_x;
-			
-			d = d + (2 * dy);
+			// d = d + (2 * dy);
+			d = d + (dy << 1);
 			if (d > 0) 
 			{
 				d = d - (2 * dx);
 				y = y + s_y;
 			}
-			max_delta--;
 		}
 	}
 	else
@@ -71,10 +70,10 @@ void 	ft_bresenham(int x_0, int x_1, int y_0, int y_1, void *mlx_ptr, void *win_
 		int y = y_1;
 		int d = -dy;
 
-		max_delta = max_delta + 1;
-		while(max_delta != 0)
+		max_delta++;
+		while(max_delta--)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 8388352);
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
 			y = y + s_y;
 			d = d + 2 * dx;
 			if (d > 0)
@@ -82,7 +81,6 @@ void 	ft_bresenham(int x_0, int x_1, int y_0, int y_1, void *mlx_ptr, void *win_
 				d = d - (2 * dy);
 				x = x + s_x;
 			}
-			max_delta--;
         }
 	}
 }
--- go_MinilibX/main.c
+++ go_MinilibX/main.c
@@ -15,69 +15,97 @@ int		ft_max(int d_x, int d_y, int max_d)
 	return (max_d);
 }
 
-void 	ft_bresenham(int x_0, int x_1, int y_0, int y_1, void *mlx_ptr, void *win_ptr)
+void	form_line_below(int x_0, int y_0, int x_1, int y_1, int dx, int dy, int yi, void *mlx_ptr, void *win_ptr, int color)
 {
-	int 	dx;
-	int		dy;
-	int 	s_x;
-	int 	s_y;
-	int 	max_delta;
+	int difference;
+	int y;
 
-	dx = abs(x_1 - x_0);
-	if ((x_1 - x_0) >= 0)
-		s_x = 1;
-	else
-		s_x = 0;
-    dy = abs(y_1 - y_0);
-    if ((y_1 - y_0) >= 0)
-		s_y = 1;
-	else
-		s_y = 0; 
-    max_delta = ft_max(dx, dy, max_delta);    
-    if (max_delta == 0)
-		mlx_pixel_put(mlx_ptr, win_ptr, x_0, y_0, 8388352);
-	if (dy <= dx)
+	difference = 0;
+	y = 0;
+	if (dy < 0)
 	{
-		printf("y <= dx\n");
-		int x = x_0;
-		int y = y_0;
-		int d = -dx;
-
-		max_delta++;
-		while(max_delta--)
-		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 8388352);
-			printf("%d %d\n",x, y);
-			x = x + s_x;
-			d = d + (2 * dy);
-			if (d > 0) 
-			{
-				d = d - (2 * dx);
-				y = y + s_y;
-			}
-		}
+		yi = -1;
+		dy = -dy;
 	}
-	else
-	{
-		int x = x_1;
-		int y = y_1;
-		int d = -dy;
+	difference = (2 * dy) - dx;
+	y = y_0;
 
-		max_delta++;
-		while(max_delta--)
+	int x = x_0;
+	while (x < x_1)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+		if (difference > 0)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 8388352);
-			y = y + s_y;
-			d = d + 2 * dx;
-			if (d > 0)
-			{
-				d = d - (2 * dy);
-				x = x + s_x;
-			}
-        }
+			y = y + yi;
+       		difference = difference - (2 * dx);
+		}
+		difference = difference + (2 * dy);
+		x++;
 	}
 }
 
+void	form_line_above(int x_0, int y_0, int x_1, int y_1, int dx, int dy, int xi, void *mlx_ptr, void *win_ptr, int color)
+{
+	int difference;
+	int x;
+
+	difference = 0;
+	x = 0;
+	if (dx < 0)
+	{
+		xi = -1;
+		dx = -dx;
+	}
+	difference = (2 * dx) - dy;
+	x = x_0;
+
+	int y = y_0;
+	while (y < y_1)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+		if (difference > 0)
+		{
+			x = x + xi;
+       		difference = difference - (2 * dy);
+		}
+		difference = difference + (2 * dx);
+		y++;
+	}
+	
+}
+
+void 		ft_bresenham(int x_0, int y_0, int x_1, int y_1, void *mlx_ptr, void *win_ptr, int color)
+{
+	int 	dx;
+	int		dy;
+	int 	xi;
+	int 	yi;
+	
+	dx = abs(x_1 - x_0);
+	dy = abs(y_1 - y_0);
+	xi = 1;
+	yi = 1;
+	if (dx == dy && dx <= 1 && dy <= 1)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, x_0, y_0, color);
+		return ;
+	}
+	else if (dy < dx)
+	{
+		if (x_0 > x_1)
+      		form_line_below(x_1, y_1, x_0, y_0, dx, dy, yi, mlx_ptr, win_ptr, color);
+    	else
+      		form_line_below(x_0, y_0, x_1, y_1, dx, dy, yi, mlx_ptr, win_ptr, color);
+	}
+	else if (dy > dx)
+	{
+		if (y_0 > y_1)
+      		form_line_above(x_1, y_1, x_0, y_0, dx, dy, xi, mlx_ptr, win_ptr, color);
+    	else
+      		form_line_above(x_0, y_0, x_1, y_1, dx, dy, xi, mlx_ptr, win_ptr, color);
+	}
+
+}
 
 void		ft_putchar(char c)
 {
@@ -94,12 +122,22 @@ int			main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	int color_1;
+	int color_2;
+	int color_3;
 
+	color_1 = 16777215;
+	color_2 = 16711680;
+	color_3 = 8388352;
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
 	// mlx_pixel_put(mlx_ptr, win_ptr, 50, 40, 8388352);
 	// mlx_key_hook(win_ptr, deal_key, (void *)0);
-	ft_bresenham(3, 20, 6, 2, mlx_ptr, win_ptr);
+
+	ft_bresenham(10, 50, 60, 20, mlx_ptr, win_ptr, color_1);
+	// ft_bresenham(20, 13, 40, 80, mlx_ptr, win_ptr, color_2);
+	// ft_bresenham(80, 40 , 13, 20, mlx_ptr, win_ptr, color_3);
+	
 	mlx_loop(mlx_ptr);
 	return (0);
 }
