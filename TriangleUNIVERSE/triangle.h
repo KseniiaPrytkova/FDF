646159d6d0e19fa4af2de1cb0724deef55381bdf
Kseniia <prytkovakseniia@gmail.com>
2018-05-27 15:34:10 +0300

    Update triangle.h

--- TriangleUNIVERSE/triangle.h
+++ TriangleUNIVERSE/triangle.h
@@ -27,37 +27,37 @@
 # define ARG_TO_X	e->x1
 # define ARG_TO_Y	e->y1
 
-typedef struct	s_point
+typedef struct		s_point
 {
-	int			x;
-	int			y;
-	int			z;
-	int			x_before;
-	int			y_before;
-	int			z_before;
-	int			color;
-}				t_point;
+	int		x;
+	int		y;
+	int		z;
+	int		x_before;
+	int		y_before;
+	int		z_before;
+	int		color;
+}			t_point;
 
-typedef struct	s_env
+typedef struct		s_env
 {
 	void		*mlx;
 	void		*win;
-	int			step_x;
-	int			step_y;
-	int			dx;
-	int			dy;
-	int			x0;
-	int			x1;
-	int			y0;
-	int			y1;
-	int			line_color;
-	int			tmp_angle;
+	int		step_x;
+	int		step_y;
+	int		dx;
+	int		dy;
+	int		x0;
+	int		x1;
+	int		y0;
+	int		y1;
+	int		line_color;
+	int		tmp_angle;
 	t_point		*points;
 
-}				t_env;
+}			t_env;
 
 void			bresenham_line(t_env *e);
-int				key_exit(int keycode);
+int			key_exit(int keycode);
 int 			key_draw(int keycode, t_env *e);
 int 			loop_draw(t_env *e);
 void			rotate_x(t_env *e, int angle);
